182b4d858c29ecc9cbe8bcf17a322b8ede3aad54
Nguyen Van A <kousei2k4@gmail.com>
2025-05-15 13:52:27 +0700

    Thực hành MVC

---
+++ FormDangKy/src/main/java/com/example/register/RegisterController.java
@@ -0,0 +1,22 @@
+package com.example.register;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegisterController {
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String submitForm(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("message", "Đăng ký thành công!");
+        return "register";
+    }
+} 
